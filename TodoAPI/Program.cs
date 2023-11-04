007b456e63d4fabd1588c570420663ec6d47d77b
Maksym Vasylenko <maks.fa.27@gmail.com>
2023-11-05 01:40:28 +0200

    added context & minor fixes WIP

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -1,11 +1,22 @@
-using TodoAPI.Infrastructure.StartupConfiguration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Todo.DataAccess;
+using Todo.DataAccess.Configuration;
+using Todo.DataAccess.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.ConfigureDbContext(builder.Configuration);
+
+//builder.Services.ConfigureDbContext(builder.Configuration);
+//builder.Services.AddIdentityConfiguration();
+
+builder.Services.AddDbContext<ApplicationContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
